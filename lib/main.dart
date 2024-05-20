ec8d5b12ddbd17671db131f467b0ea22ffe15b6c
Devender Butani <devenderbutani21@gmail.com>
2024-05-19 22:34:32 -0400

    Font, Image and Format Changes

--- lib/main.dart
+++ lib/main.dart
@@ -37,15 +37,166 @@ class MyHomePage extends StatelessWidget {
             Row(
               children: [
                 CircleAvatar(
-                  radius: 40,
+                  radius: 60,
+                  backgroundImage: AssetImage(
+                    "assets/images/img.jpg",
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
                 ),
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Name"),
-                    Text("Designation"),
+                    Text(
+                      "Devender Butani",
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontFamily: "assets/fonts/Roboto-Medium.ttf",
+                      ),
+                    ),
+                    Text(
+                      "Software Developer",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
                   ],
                 ),
               ],
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                left: 30.0,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.school,
+                        size: 48,
+                      ),
+                      SizedBox(
+                        width: 30,
+                      ),
+                      Text(
+                        "University of Windsor",
+                        style: TextStyle(
+                          fontSize: 24,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.computer_rounded,
+                        size: 48,
+                      ),
+                      SizedBox(
+                        width: 30,
+                      ),
+                      Text(
+                        "School Name",
+                        style: TextStyle(
+                          fontSize: 24,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.location_city,
+                        size: 48,
+                      ),
+                      SizedBox(
+                        width: 30,
+                      ),
+                      Text(
+                        "School Name",
+                        style: TextStyle(
+                          fontSize: 24,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.email,
+                        size: 48,
+                      ),
+                      SizedBox(
+                        width: 30,
+                      ),
+                      Text(
+                        "School Name",
+                        style: TextStyle(
+                          fontSize: 24,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        size: 48,
+                      ),
+                      SizedBox(
+                        width: 30,
+                      ),
+                      Text(
+                        "School Name",
+                        style: TextStyle(
+                          fontSize: 24,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: EdgeInsets.all(
+                10.0,
+              ),
+              child: Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel urna nec purus facilisis cursus. Curabitur tincidunt sem ac eros gravida, ac placerat libero dignissim.",
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 90,
+            ),
+            Text(
+              "Created by Devender Butani",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
             ),
           ],
         ),
